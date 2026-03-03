2546f78bd2ea62717e197a5abe771b635a822d87
CiamalaKombeGreg <he202098@students.ephec.be>
2026-03-03 17:44:39 +0100

    Implemented Compiler's lexer part + Added tests to TestLexer.java

--- test/TestLexer.java
+++ test/TestLexer.java
@@ -1,17 +1,37 @@
+import java.io.IOException;
+import java.io.StringReader;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
-import java.io.StringReader;
 import compiler.Lexer.Lexer;
+import compiler.Lexer.Symbol;
+import compiler.Lexer.SymbolType;
 
 public class TestLexer {
     
     @Test
-    public void test() {
-        String input = "var x int = 2;";
+    public void test() throws IOException {
+        String input = "private final INT x";
         StringReader reader = new StringReader(input);
         Lexer lexer = new Lexer(reader);
         assertNotNull(lexer.getNextSymbol());
+
+        Symbol s1 = lexer.getNextSymbol();
+        assertEquals(SymbolType.KEYWORD, s1.getType());
+        assertEquals("final", s1.getValue());
+
+        Symbol s2 = lexer.getNextSymbol();
+        assertEquals(SymbolType.KEYWORD, s2.getType());
+        assertEquals("INT", s2.getValue());
+
+        Symbol s3 = lexer.getNextSymbol();
+        assertEquals(SymbolType.IDENTIFIER, s3.getType());
+        assertEquals("x", s3.getValue());
+
+        Symbol s4 = lexer.getNextSymbol();
+        assertEquals(SymbolType.EOF, s4.getType());
     }
 
 }
